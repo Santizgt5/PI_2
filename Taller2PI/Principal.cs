fb025ed4d2871d6ed7220e2f2f96dd378eeee44c
Santiago Alvarez Rivera <santiago.alvarez2@correo.icesi.edu.co>
2019-02-03 17:16:16 -0500

    Add model of proyect wit the class ZonaMilitar

---
+++ Taller2PI/Principal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2PI
+{
+    class Principal
+    {
+
+        private ArrayList datos;
+
+        public Principal()
+        {
+            Datos = new ArrayList();
+
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("..\\..\\data.csv");
+                while((line = sr.ReadLine()) != null)
+                {
+                    String[] d = line.Split(',');
+                    int z = Int32.Parse(d[0]);
+                    ZonaMilitar zm = new ZonaMilitar(z, d[1], d[2], d[3], d[4]);
+                    datos.Add(zm);
+                }
+
+            }catch(Exception e)
+            {
+
+            }
+        }
+
+        public ArrayList Datos { get => datos; set => datos = value; }
+
+        /*public static void Main(String[] args)
+        {
+            Principal e = new Principal();
+            for (int i = 0; i < e.datos.Count; i++)
+            {
+                ZonaMilitar m =  (ZonaMilitar) e.datos[i];
+                Console.WriteLine("{1},{2},{3},{4},{5},", m.Zona, m.ZonaDim, m.Direccion, m.Telefono, m.Location);
+            }
+        }
+        */
+    }
+
+    
+
+
+}
---
+++ Taller2PI/ZonaMilitar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2PI
+{
+    class ZonaMilitar
+    {
+
+        private int zona;
+        private string zonaDim;
+        private string direccion;
+        private string telefono;
+        private string location;
+
+        public ZonaMilitar(int zona, string zonaDim, string direccion, string telefono, string location)
+        {
+            this.Zona = zona;
+            this.ZonaDim = zonaDim;
+            this.Direccion = direccion;
+            this.Telefono = telefono;
+            this.Location = location;
+        }
+
+        public int Zona { get => zona; set => zona = value; }
+        public string ZonaDim { get => zonaDim; set => zonaDim = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Location { get => location; set => location = value; }
+    }
+}
